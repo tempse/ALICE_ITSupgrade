8e469744c8e2026e4f3deeba0902de1dc05d0de8
Sebastian Templ <sebastian.templ@cern.ch>
2017-02-13 11:34:47 +0100

    Add macro 'AssignNewDcaValues.C'
    This macro copies the input tree, but changes the DCAxy values of
    HF events randomly and accordingly to the DCAxy distributions
    extracted from data (using 'ExtractDcaHistos.C').

---
+++ macros/AssignNewDcaValues.C
@@ -0,0 +1,272 @@
+#include <iostream>
+
+#include <TString.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TRandom.h>
+#include <TStopwatch.h>
+
+
+
+void AssignNewDcaValues() {
+
+  TString infile_name = "../pairTrees/FT2_AnalysisResults_Upgrade_all-Ev_pairtree_us/FT2_AnalysisResults_Upgrade_all-Ev_pairtree_us_train_1-100-split.root";
+  
+  TString outfile_name = "temp_fakeDcaTree.root";//"../pairTrees/FT2_AnalysisResults_Upgrade_all-Ev_pairtree_us/FT2_AnalysisResults_Upgrade_all-Ev_pairtree_us_train_1-100-split_fakeDcaValues.root";
+
+
+  // definition of Gaussian fit parameters of DCAxy distributions:
+  const Int_t dcaxy1_ccbar_mu    =  1.77e-4;
+  const Int_t dcaxy1_ccbar_sigma =  9.07e-3;
+  const Int_t dcaxy2_ccbar_mu    = -1.24e-4;
+  const Int_t dcaxy2_ccbar_sigma =  7.40e-3;
+  const Int_t dcaxy1_bbbar_mu    =  8.26e-5;
+  const Int_t dcaxy1_bbbar_sigma = -5.86e-3;
+  const Int_t dcaxy2_bbbar_mu    = -4.07e-4;
+  const Int_t dcaxy2_bbbar_sigma = -1.06e-2;
+
+  
+  TFile *infile = new TFile(infile_name, "READ");
+  TTree *input_tree = (TTree*)infile->Get("pairTree_us");
+
+  Int_t in_trackid1, in_trackid2;
+  Int_t in_eventid1, in_eventid2;
+  Int_t in_isrp;
+  Int_t in_isus;
+  Int_t in_isconv;
+  Int_t in_ishf;
+  Int_t in_iscorrcharm;
+  Int_t in_iscorrbottom;
+  Int_t in_iscorrcharmfrombottom;
+  Int_t in_chargesign;
+  Float_t in_opang;
+  Float_t in_diffz;
+  Float_t in_mass;
+  Float_t in_sumz;
+  Float_t in_phiv;
+  Float_t in_px1, in_px2;
+  Float_t in_py1, in_py2;
+  Float_t in_pz1, in_pz2;
+  Float_t in_mcpx1, in_mcpx2;
+  Float_t in_mcpy1, in_mcpy2;
+  Float_t in_mcpz1, in_mcpz2;
+  Int_t in_pdg1, in_pdg2;
+  Int_t in_motherpdg1, in_motherpdg2;
+  Float_t in_dcaxy1, in_dcaxy2;
+  Float_t in_dcaz1, in_dcaz2;
+  Int_t in_nits1, in_nits2;
+  Float_t in_phi1, in_phi2;
+  Float_t in_eta1, in_eta2;
+  Float_t in_pt1, in_pt2;
+  input_tree->SetBranchAddress("TrackID1", &in_trackid1);
+  input_tree->SetBranchAddress("TrackID2", &in_trackid2);
+  input_tree->SetBranchAddress("EventID1", &in_eventid1);
+  input_tree->SetBranchAddress("EventID2", &in_eventid2);
+  input_tree->SetBranchAddress("IsRP", &in_isrp);
+  input_tree->SetBranchAddress("IsUS", &in_isus);
+  input_tree->SetBranchAddress("IsConv", &in_isconv);
+  input_tree->SetBranchAddress("IsHF", &in_ishf);
+  input_tree->SetBranchAddress("IsCorrCharm", &in_iscorrcharm);
+  input_tree->SetBranchAddress("IsCorrBottom", &in_iscorrbottom);
+  input_tree->SetBranchAddress("IsCorrCharmFromBottom", &in_iscorrcharmfrombottom);
+  input_tree->SetBranchAddress("ChargeSign", &in_chargesign);
+  input_tree->SetBranchAddress("opang", &in_opang);
+  input_tree->SetBranchAddress("diffz", &in_diffz);
+  input_tree->SetBranchAddress("mass", &in_mass);
+  input_tree->SetBranchAddress("sumz", &in_sumz);
+  input_tree->SetBranchAddress("phiv", &in_phiv);
+  input_tree->SetBranchAddress("px1", &in_px1);
+  input_tree->SetBranchAddress("px2", &in_px2);
+  input_tree->SetBranchAddress("py1", &in_py1);
+  input_tree->SetBranchAddress("py2", &in_py2);
+  input_tree->SetBranchAddress("pz1", &in_pz1);
+  input_tree->SetBranchAddress("pz2", &in_pz2);
+  input_tree->SetBranchAddress("mcPx1", &in_mcpx1);
+  input_tree->SetBranchAddress("mcPx2", &in_mcpx2);
+  input_tree->SetBranchAddress("mcPy1", &in_mcpy1);
+  input_tree->SetBranchAddress("mcPy2", &in_mcpy2);
+  input_tree->SetBranchAddress("mcPz1", &in_mcpz1);
+  input_tree->SetBranchAddress("mcPz2", &in_mcpz2);
+  input_tree->SetBranchAddress("pdg1", &in_pdg1);
+  input_tree->SetBranchAddress("pdg2", &in_pdg2);
+  input_tree->SetBranchAddress("motherPdg1", &in_motherpdg1);
+  input_tree->SetBranchAddress("motherPdg2", &in_motherpdg2);
+  input_tree->SetBranchAddress("DCAxy1", &in_dcaxy1);
+  input_tree->SetBranchAddress("DCAxy2", &in_dcaxy2);
+  input_tree->SetBranchAddress("DCAz1", &in_dcaz1);
+  input_tree->SetBranchAddress("DCAz2", &in_dcaz2);
+  input_tree->SetBranchAddress("nITS1", &in_nits1);
+  input_tree->SetBranchAddress("nITS2", &in_nits2);
+  input_tree->SetBranchAddress("phi1", &in_phi1);
+  input_tree->SetBranchAddress("phi2", &in_phi2);
+  input_tree->SetBranchAddress("eta1", &in_eta1);
+  input_tree->SetBranchAddress("eta2", &in_eta2);
+  input_tree->SetBranchAddress("pt1", &in_pt1);
+  input_tree->SetBranchAddress("pt2", &in_pt2);
+
+
+  
+  TFile *outfile = new TFile(outfile_name, "RECREATE");
+  TTree *output_tree = new TTree("pairTree_us","pairTree_us");
+  
+  Int_t out_trackid1, out_trackid2;
+  Int_t out_eventid1, out_eventid2;
+  Int_t out_isrp;
+  Int_t out_isus;
+  Int_t out_isconv;
+  Int_t out_ishf;
+  Int_t out_iscorrcharm;
+  Int_t out_iscorrbottom;
+  Int_t out_iscorrcharmfrombottom;
+  Int_t out_chargesign;
+  Float_t out_opang;
+  Float_t out_diffz;
+  Float_t out_mass;
+  Float_t out_sumz;
+  Float_t out_phiv;
+  Float_t out_px1, out_px2;
+  Float_t out_py1, out_py2;
+  Float_t out_pz1, out_pz2;
+  Float_t out_mcpx1, out_mcpx2;
+  Float_t out_mcpy1, out_mcpy2;
+  Float_t out_mcpz1, out_mcpz2;
+  Int_t out_pdg1, out_pdg2;
+  Int_t out_motherpdg1, out_motherpdg2;
+  Float_t out_dcaxy1, out_dcaxy2;
+  Float_t out_dcaz1, out_dcaz2;
+  Int_t out_nits1, out_nits2;
+  Float_t out_phi1, out_phi2;
+  Float_t out_eta1, out_eta2;
+  Float_t out_pt1, out_pt2;
+  output_tree->Branch("TrackID1", &out_trackid1);
+  output_tree->Branch("TrackID2", &out_trackid2);
+  output_tree->Branch("EventID1", &out_eventid1);
+  output_tree->Branch("EventID2", &out_eventid2);
+  output_tree->Branch("IsRP", &out_isrp);
+  output_tree->Branch("IsUS", &out_isus);
+  output_tree->Branch("IsConv", &out_isconv);
+  output_tree->Branch("IsHF", &out_ishf);
+  output_tree->Branch("IsCorrCharm", &out_iscorrcharm);
+  output_tree->Branch("IsCorrBottom", &out_iscorrbottom);
+  output_tree->Branch("IsCorrCharmFromBottom", &out_iscorrcharmfrombottom);
+  output_tree->Branch("ChargeSign", &out_chargesign);
+  output_tree->Branch("opang", &out_opang);
+  output_tree->Branch("diffz", &out_diffz);
+  output_tree->Branch("mass", &out_mass);
+  output_tree->Branch("sumz", &out_sumz);
+  output_tree->Branch("phiv", &out_phiv);
+  output_tree->Branch("px1", &out_px1);
+  output_tree->Branch("px2", &out_px2);
+  output_tree->Branch("py1", &out_py1);
+  output_tree->Branch("py2", &out_py2);
+  output_tree->Branch("pz1", &out_pz1);
+  output_tree->Branch("pz2", &out_pz2);
+  output_tree->Branch("mcPx1", &out_mcpx1);
+  output_tree->Branch("mcPx2", &out_mcpx2);
+  output_tree->Branch("mcPy1", &out_mcpy1);
+  output_tree->Branch("mcPy2", &out_mcpy2);
+  output_tree->Branch("mcPz1", &out_mcpz1);
+  output_tree->Branch("mcPz2", &out_mcpz2);
+  output_tree->Branch("pdg1", &out_pdg1);
+  output_tree->Branch("pdg2", &out_pdg2);
+  output_tree->Branch("motherPdg1", &out_motherpdg1);
+  output_tree->Branch("motherPdg2", &out_motherpdg2);
+  output_tree->Branch("DCAxy1", &out_dcaxy1);
+  output_tree->Branch("DCAxy2", &out_dcaxy2);
+  output_tree->Branch("DCAz1", &out_dcaz1);
+  output_tree->Branch("DCAz2", &out_dcaz2);
+  output_tree->Branch("nITS1", &out_nits1);
+  output_tree->Branch("nITS2", &out_nits2);
+  output_tree->Branch("phi1", &out_phi1);
+  output_tree->Branch("phi2", &out_phi2);
+  output_tree->Branch("eta1", &out_eta1);
+  output_tree->Branch("eta2", &out_eta2);
+  output_tree->Branch("pt1", &out_pt1);
+  output_tree->Branch("pt2", &out_pt2);
+
+  TRandom *rand = new TRandom();
+  
+  
+  Int_t num_entries = input_tree->GetEntries();
+
+  TStopwatch *watch = new TStopwatch();
+  
+  std::cout << std::endl;
+  watch->Start();
+  for(Int_t entry=0; entry<num_entries; entry++) {
+    if((entry%1000)==0) std::cout << "\rProcessing entry " << entry << " of "
+				  << num_entries << " (" << entry*100/num_entries << " %)...";
+    input_tree->GetEntry(entry);
+
+    out_trackid1 = in_trackid1;
+    out_trackid2 = in_trackid2;
+    out_eventid1 = in_eventid1;
+    out_eventid2 = in_eventid2;
+    out_isrp = in_isrp;
+    out_isus = in_isus;
+    out_isconv = in_isconv;
+    out_ishf = in_ishf;
+    out_iscorrcharm = in_iscorrcharm;
+    out_iscorrbottom = in_iscorrbottom;
+    out_iscorrcharmfrombottom = in_iscorrcharmfrombottom;
+    out_chargesign = in_chargesign;
+    out_opang = in_opang;
+    out_diffz = in_diffz;
+    out_mass = in_mass;
+    out_sumz = in_sumz;
+    out_phiv = in_phiv;
+    out_px1 = in_px1;
+    out_px2 = in_px2;
+    out_py1 = in_py1;
+    out_py2 = in_py2;
+    out_pz1 = in_pz1;
+    out_pz2 = in_pz2;
+    out_mcpx1 = in_mcpx1;
+    out_mcpx2 = in_mcpx2;
+    out_mcpy1 = in_mcpy1;
+    out_mcpy2 = in_mcpy2;
+    out_mcpz1 = in_mcpz1;
+    out_mcpz2 = in_mcpz2;
+    out_pdg1 = in_pdg1;
+    out_pdg2 = in_pdg2;
+    out_motherpdg1 = in_motherpdg1;
+    out_motherpdg2 = in_motherpdg2;
+    out_dcaxy1 = in_dcaxy1;
+    out_dcaxy2 = in_dcaxy2;
+    out_dcaz1 = in_dcaz1;
+    out_dcaz2 = in_dcaz2;
+    out_nits1 = in_nits1;
+    out_nits2 = in_nits2;
+    out_eta1 = in_eta1;
+    out_eta2 = in_eta2;
+    out_pt1 = in_pt1;
+    out_pt2 = in_pt2;
+
+    // assign fake DCAxy values to HF events:
+    if(in_ishf == 1) {
+      if(in_iscorrcharm == 1) {
+	out_dcaxy1 = rand->Gaus(dcaxy1_ccbar_mu, dcaxy1_ccbar_sigma);
+	out_dcaxy2 = rand->Gaus(dcaxy2_ccbar_mu, dcaxy2_ccbar_sigma);
+      }
+      if(in_iscorrbottom == 1) {
+	out_dcaxy1 = rand->Gaus(dcaxy1_bbbar_mu, dcaxy1_bbbar_sigma);
+	out_dcaxy2 = rand->Gaus(dcaxy2_bbbar_mu, dcaxy2_bbbar_sigma);
+      }
+    }
+    
+
+    output_tree->Fill();
+  }
+  watch->Stop();
+  std::cout << "\rProcessing entry " << num_entries << " of " << num_entries
+	    << " (100 %)" << std::endl << std::endl;
+
+  std::cout << "Time passed since begin of processing: ";
+  watch->Print();
+  std::cout << std::endl;
+
+  output_tree->Write();
+
+  std::cout << "Created file " << outfile->GetName() << std::endl;
+  
+}
