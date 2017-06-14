df2e519a5c4ee1692f88f949bde1812ada8e1547
Sebastian Templ <sebastian.templ@cern.ch>
2017-06-14 16:06:00 +0200

    Extend plotting macro
    Before: Only one plot considering the entire mass range.
    Now: Create two equivalent plots for (1) the entire mass range and
    (2) for masses greater than 50 MeV/c^2.

--- macros/PlotROC_PIDeffs.py
+++ macros/PlotROC_PIDeffs.py
@@ -8,6 +8,9 @@ import root_numpy
 from sklearn.metrics import roc_curve, roc_auc_score
 
 
+################################################################################
+
+
 filename_MCdata = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/workingData/FT2_AnalysisResults_Upgrade_DCAvec_PIDeffs_pairtree_us_part2_1-9-split.root"
 
 branches_MCdata = [
@@ -19,19 +22,30 @@ branches_MCdata = [
 ]
 
 
-filename_MVAoutput_woPIDeffs = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/sklearn_BDT_analysis/randomForest/woSampleWeights_PIDeffs_noMassCuts/clf_predict/predictions_BDT.root"
+filename_MVAoutput_woPIDeffs_noMassCuts = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/sklearn_BDT_analysis/randomForest/woSampleWeights_PIDeffs_noMassCuts/clf_predict/predictions_BDT.root"
 
-branches_MVAoutput_woPIDeffs = ['BDT']
+branches_MVAoutput_woPIDeffs_noMassCuts = ['BDT']
 
 
-filename_MVAoutput_wPIDeffs = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/sklearn_BDT_analysis/randomForest/wSampleWeights_PIDeffs_noMassCuts/clf_predict/predictions_BDT.root"
+filename_MVAoutput_wPIDeffs_noMassCuts = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/sklearn_BDT_analysis/randomForest/wSampleWeights_PIDeffs_noMassCuts/clf_predict/predictions_BDT.root"
 
-branches_MVAoutput_wPIDeffs = ['BDT']
+branches_MVAoutput_wPIDeffs_noMassCuts = ['BDT']
+
+
+filename_MVAoutput_woPIDeffs_massCut = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/sklearn_BDT_analysis/randomForest/woSampleWeights_PIDeffs_0.5mass/clf_predict/predictions_BDT.root"
+
+branches_MVAoutput_woPIDeffs_massCut = ['BDT']
+
+filename_MVAoutput_wPIDeffs_massCut = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/sklearn_BDT_analysis/randomForest/wSampleWeights_PIDeffs_0.5mass/clf_predict/predictions_BDT.root"
+
+branches_MVAoutput_wPIDeffs_massCut = ['BDT']
+
 
 
 start = 0
 stop = 75000000
 
+massCut = .05
 
 fig_width = 6.83
 fig_height = 5.12
@@ -41,49 +55,54 @@ color_woPIDeffs = 'black'
 color_wPIDeffs = 'orange'
 
 
+################################################################################
+
+
 df_MCdata = pd.DataFrame(root_numpy.root2array(filename_MCdata,
                                                branches=branches_MCdata,
                                                start=start,
                                                stop=stop))
 
-df_MVAoutput_woPIDeffs = pd.DataFrame(root_numpy.root2array(filename_MVAoutput_woPIDeffs,
-                                                            branches=branches_MVAoutput_woPIDeffs,
-                                                            start=start,
-                                                            stop=stop))
+##### entire mass range #####
 
-df_MVAoutput_wPIDeffs = pd.DataFrame(root_numpy.root2array(filename_MVAoutput_wPIDeffs,
-                                                           branches=branches_MVAoutput_wPIDeffs,
-                                                           start=start,
-                                                           stop=stop))
+df_MVAoutput_woPIDeffs_noMassCuts = pd.DataFrame(root_numpy.root2array(filename_MVAoutput_woPIDeffs_noMassCuts,
+                                                                       branches=branches_MVAoutput_woPIDeffs_noMassCuts,
+                                                                       start=start,
+                                                                       stop=stop))
+
+df_MVAoutput_wPIDeffs_noMassCuts = pd.DataFrame(root_numpy.root2array(filename_MVAoutput_wPIDeffs_noMassCuts,
+                                                                      branches=branches_MVAoutput_wPIDeffs_noMassCuts,
+                                                                      start=start,
+                                                                      stop=stop))
 
 
 sample_weight = (df_MCdata['PIDeff1']*df_MCdata['PIDeff2']).values.astype(np.float32)
 
 
 
-fpr_woPIDeffs, tpr_woPIDeffs, thresholds_woPIDeffs = roc_curve(df_MCdata['IsConv'],
-                                                               df_MVAoutput_woPIDeffs['BDT'],
-                                                               pos_label=0)
-roc_auc_woPIDeffs = roc_auc_score(df_MCdata['IsConv'],
-                                  df_MVAoutput_woPIDeffs['BDT'])
+fpr_woPIDeffs_noMassCuts, tpr_woPIDeffs_noMassCuts, thresholds_woPIDeffs_noMassCuts = roc_curve(df_MCdata['IsConv'],
+                                                                                                df_MVAoutput_woPIDeffs_noMassCuts['BDT'],
+                                                                                                pos_label=0)
+roc_auc_woPIDeffs_noMassCuts = roc_auc_score(df_MCdata['IsConv'],
+                                             df_MVAoutput_woPIDeffs_noMassCuts['BDT'])
 
 
-fpr_wPIDeffs, tpr_wPIDeffs, thresholds_wPIDeffs = roc_curve(df_MCdata['IsConv'],
-                                                            df_MVAoutput_wPIDeffs['BDT'],
-                                                            pos_label=0,
-                                                            sample_weight=sample_weight)
-roc_auc_wPIDeffs = roc_auc_score(df_MCdata['IsConv'],
-                                 df_MVAoutput_wPIDeffs['BDT'],
-                                 sample_weight=sample_weight)
+fpr_wPIDeffs_noMassCuts, tpr_wPIDeffs_noMassCuts, thresholds_wPIDeffs_noMassCuts = roc_curve(df_MCdata['IsConv'],
+                                                                                             df_MVAoutput_wPIDeffs_noMassCuts['BDT'],
+                                                                                             pos_label=0,
+                                                                                             sample_weight=sample_weight)
+roc_auc_wPIDeffs_noMassCuts = roc_auc_score(df_MCdata['IsConv'],
+                                            df_MVAoutput_wPIDeffs_noMassCuts['BDT'],
+                                            sample_weight=sample_weight)
 
 
 plt.figure(figsize=(fig_width,fig_height), dpi=fig_dpi)
-plt.plot(fpr_woPIDeffs, tpr_woPIDeffs,
-         label='excl. PID efficiences (area = %.3f)' % (1.-roc_auc_woPIDeffs),
+plt.plot(fpr_woPIDeffs_noMassCuts, tpr_woPIDeffs_noMassCuts,
+         label='excl. PID efficiences (area = %.3f)' % (1.-roc_auc_woPIDeffs_noMassCuts),
          alpha=.85,
          color=color_woPIDeffs)
-plt.plot(fpr_wPIDeffs, tpr_wPIDeffs,
-         label='incl. PID efficiencies (area = %.3f)' % (1.-roc_auc_wPIDeffs),
+plt.plot(fpr_wPIDeffs_noMassCuts, tpr_wPIDeffs_noMassCuts,
+         label='incl. PID efficiencies (area = %.3f)' % (1.-roc_auc_wPIDeffs_noMassCuts),
          alpha=.85,
          color=color_wPIDeffs)
 plt.plot([0,1], [0,1], 'k--')
@@ -93,5 +112,66 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('Receiver operating characteristic curve (no mass cuts)')
 plt.legend(loc=4)
-plt.savefig('temp_output/ROCcurves_PIDeffs_CombConvRej_masscut.png')
-plt.savefig('temp_output/ROCcurves_PIDeffs_CombConvRej_masscut.pdf')
+plt.savefig('temp_output/ROCcurves_PIDeffs_CombConvRej_noMassCuts.png')
+plt.savefig('temp_output/ROCcurves_PIDeffs_CombConvRej_noMassCuts.pdf')
+
+
+
+
+##### mass > 50 MeV/c^2 #####
+
+
+df_MVAoutput_woPIDeffs_massCut = pd.DataFrame(root_numpy.root2array(filename_MVAoutput_woPIDeffs_massCut,
+                                                                    branches=branches_MVAoutput_woPIDeffs_massCut,
+                                                                    start=start,
+                                                                    stop=stop))
+
+df_MVAoutput_wPIDeffs_massCut = pd.DataFrame(root_numpy.root2array(filename_MVAoutput_wPIDeffs_massCut,
+                                                                   branches=branches_MVAoutput_wPIDeffs_massCut,
+                                                                   start=start,
+                                                                   stop=stop))
+
+
+sample_weight = (df_MCdata['PIDeff1']*df_MCdata['PIDeff2']).values.astype(np.float32)
+
+
+# process data according to mass cuts:
+df_MVAoutput_woPIDeffs_massCut = df_MVAoutput_woPIDeffs_massCut[np.array(df_MCdata['mass']>massCut)]
+df_MVAoutput_wPIDeffs_massCut = df_MVAoutput_wPIDeffs_massCut[np.array(df_MCdata['mass']>massCut)]
+df_MCdata = df_MCdata[np.array(df_MCdata['mass']>massCut)]
+
+
+fpr_woPIDeffs_massCut, tpr_woPIDeffs_massCut, thresholds_woPIDeffs_massCut = roc_curve(df_MCdata['IsConv'], #((df_MCdata['IsRP']==0) & (df_MCdata['IsConv']==1)).astype(int)
+                                                                                       df_MVAoutput_woPIDeffs_massCut['BDT'],
+                                                                                       pos_label=0)
+roc_auc_woPIDeffs_massCut = roc_auc_score(df_MCdata['IsConv'],
+                                          df_MVAoutput_woPIDeffs_massCut['BDT'])
+
+
+fpr_wPIDeffs_massCut, tpr_wPIDeffs_massCut, thresholds_wPIDeffs_massCut = roc_curve(df_MCdata['IsConv'], #((df_MCdata['IsRP']==0) & (df_MCdata['IsConv']==1)).astype(int)
+                                                                                    df_MVAoutput_wPIDeffs_massCut['BDT'],
+                                                                                    pos_label=0,
+                                                                                    sample_weight=sample_weight)
+roc_auc_wPIDeffs_massCut = roc_auc_score(df_MCdata['IsConv'],
+                                         df_MVAoutput_wPIDeffs_massCut['BDT'],
+                                         sample_weight=sample_weight)
+
+
+plt.figure(figsize=(fig_width,fig_height), dpi=fig_dpi)
+plt.plot(fpr_woPIDeffs_massCut, tpr_woPIDeffs_massCut,
+         label='excl. PID efficiences (area = %.3f)' % (1.-roc_auc_woPIDeffs_massCut),
+         alpha=.85,
+         color=color_woPIDeffs)
+plt.plot(fpr_wPIDeffs_massCut, tpr_wPIDeffs_massCut,
+         label='incl. PID efficiencies (area = %.3f)' % (1.-roc_auc_wPIDeffs_massCut),
+         alpha=.85,
+         color=color_wPIDeffs)
+plt.plot([0,1], [0,1], 'k--')
+plt.xlim([0.0, 1.05])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic curve (mass > 50 MeV/c$^{2}$)')
+plt.legend(loc=4)
+plt.savefig('temp_output/ROCcurves_PIDeffs_CombConvRej_massCut.png')
+plt.savefig('temp_output/ROCcurves_PIDeffs_CombConvRej_massCut.pdf')
