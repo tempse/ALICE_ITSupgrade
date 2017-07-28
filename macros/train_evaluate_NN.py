e23946d086c816e803f56ab95838ba9e745162df
Sebastian Templ <sebastian.templ@cern.ch>
2017-07-28 12:56:56 +0200

    Implement logging
    Write terminal output to both the screen and a log file.

--- macros/train_evaluate_NN.py
+++ macros/train_evaluate_NN.py
@@ -1,5 +1,6 @@
 from __future__ import division
 
+import sys
 import time
 import math
 
@@ -32,6 +33,25 @@ import itertools
 # fix random seed for reproducibility
 seed = 7
 np.random.seed(seed)
+
+
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open('temp_output/ann/stdout.log', 'w')
+
+    def write(self, message):
+        self.terminal.write(message)
+	self.log.write(message)
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+	#you might want to specify some extra behavior here.
+	pass
+
+sys.stdout = Logger()
+
 
 
 # ## Data Import
