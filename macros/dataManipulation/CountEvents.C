aa7f1b9e9a04ffa200cc1effda37f752697b4426
Sebastian Templ <sebastian.templ@cern.ch>
2017-09-19 11:45:08 +0200

    Minor changes

--- macros/dataManipulation/CountEvents.C
+++ macros/dataManipulation/CountEvents.C
@@ -13,17 +13,18 @@
 void CountEvents() {
 
   // directory containing the input ROOT files
-  const char *input_dirname = "~/analysis/data/FT2_AnalysisResults_Upgrade/workingData/DNNAnalysis/";
-  const char *file_ext = "FT2_ITSup_singleTree_part2_1-9-split.root";
+  const char *input_dirname = "/home/sebastian/analysis/data/FT2_AnalysisResults_Upgrade/workingData/DNNAnalysis/";
+  const char *file_ext = "FT2_ITSup_pairTree-us_part2_538163tightCutEvents.root";
 
   // tree name
-  TString treeName = "tracks";
+  TString treeName = "pairTree_us";
 
   // branch name with event IDs in case test_sample_size is type int
-  TString ev_id_branchname = "EventID1_unique";
+  TString ev_id_branchname = "EventID1";
 
   // branch name with information about the looseness of the track cuts
-  TString trackCut_branchname = "isTrackCut";
+  TString trackCut_branchname = "TrackCut1";
+  TString trackcut_branchname_2 = "TrackCut2";
   
   TSystemDirectory input_dir(input_dirname, input_dirname);
   TList *input_files = input_dir.GetListOfFiles();
@@ -51,8 +52,10 @@ void CountEvents() {
 
   Int_t ev_id;
   Int_t TrackCut;
+  Int_t TrackCut_2;
   infileTree->SetBranchAddress(ev_id_branchname, &ev_id);
-  //infileTree->SetBranchAddress(trackCut_branchname, &TrackCut);
+  infileTree->SetBranchAddress(trackCut_branchname, &TrackCut);
+  infileTree->SetBranchAddress(trackcut_branchname_2, &TrackCut_2);
   
   Int_t infileTree_nEntries = infileTree->GetEntries();
   std::cout << "Processing " << infileTree_nEntries << " entries..." << std::endl;
@@ -65,6 +68,8 @@ void CountEvents() {
   Long64_t cnt_passedEvents = -1;
   Long64_t cnt_descendingID = 0, cnt_largeIncrement = -1, cnt_largerIncrement = -1;
 
+  Bool_t doCountEvent = kFALSE;
+
   std::cout << "Counting events..." << std::endl;
 
   for(Long64_t en=0; en<infileTree_nEntries; en++) {
@@ -75,13 +80,16 @@ void CountEvents() {
 
     infileTree->GetEntry(en);
 
+    if(TrackCut != 1 && TrackCut_2 != 1) doCountEvent = kTRUE;
+    
     if(ev_id != ev_id_prev) {
-      /*if(TrackCut == 2)*/ cnt_passedEvents++;
+      if(doCountEvent) cnt_passedEvents++;
       if(ev_id < ev_id_prev) cnt_descendingID++;
       if(ev_id != ev_id_prev+1) cnt_largeIncrement++;
       if(ev_id > ev_id_prev+2) cnt_largerIncrement++;
       
       ev_id_prev = ev_id;
+      doCountEvent = kFALSE;
     }
   }
   std::cout << std::endl;
