4adc3537e3f15ac2f868a4b723d46fc587d2f075
Sebastian Templ <sebastian.templ@cern.ch>
2018-01-15 15:48:44 +0100

    Add plotting macro
    Plots a comparison of ROC AUCs for the analyses carried out in the
    course of the HF study.

---
+++ macros/plotting/PlotROCAUCs_HFstudy.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set(rc={'figure.figsize':(11.7,8.27)})
+
+color_list = ['cerulean', 'slate blue', 'leaf green', 'scarlet']
+sns.set_palette(sns.xkcd_palette(color_list))
+
+N = 3
+
+# HF_1, HF_3, HF_2
+defaultFeats = (98.5, 76.9, 81.7)
+noMassFeat = (98.4, 0., 0.)
+noKinematicFeats = (90.8, 71.1, 71.6)
+wHFfeats = (98.4, 75.9, 0.)
+
+ind = np.arange(N) # the x locations for the groups
+width = 0.15 # the width of the bars
+
+fig, ax = plt.subplots()
+bars_defaultFeats = ax.bar(ind, defaultFeats, width, alpha=.75)
+bars_noMassFeat = ax.bar(ind+width, noMassFeat, width, alpha=.75)
+bars_noKinematicFeats = ax.bar(ind+width*2, noKinematicFeats, width, alpha=.75)
+bars_wHFfeats = ax.bar(ind+width*3, wHFfeats, width, alpha=.75)
+
+ax.set_ylabel('ROC AUC [%]')
+ax.set_xticks(ind + width*0.5)
+ax.set_xticklabels(('RP::HF','CombBG::HF','NonConvs::HF'))
+ax.legend((bars_defaultFeats[0],
+           bars_noMassFeat[0],
+           bars_noKinematicFeats[0],
+           bars_wHFfeats[0]),
+          ('defaultFeats','noMassFeat','noKinematicFeats','wHFfeats'),
+          ncol=2)
+
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.02*height,
+                '%.1f' % (height),
+                ha='center', va='bottom')
+
+autolabel(bars_defaultFeats)
+autolabel(bars_noMassFeat)
+autolabel(bars_noKinematicFeats)
+autolabel(bars_wHFfeats)
+
+plt.savefig('temp_output/HFstudy_ROCAUC-comparison.png')
+plt.savefig('temp_output/HFstudy_ROCAUC-comparison.pdf')
