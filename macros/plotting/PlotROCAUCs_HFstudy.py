8f6aa8625908bf1869879fee18779a23c40981be
Sebastian Templ <sebastian.templ@cern.ch>
2018-01-21 13:14:58 +0100

    Update HFstudy ROC AUCs plot
    - add further analyses results
    - update existing analysis results
    - change colors and patterns ("hatches") as an attempt to reflect
      some of the relations between the different analyses

--- macros/plotting/PlotROCAUCs_HFstudy.py
+++ macros/plotting/PlotROCAUCs_HFstudy.py
@@ -1,10 +1,44 @@
 import numpy as np
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set(rc={'figure.figsize':(11.7,8.27)},
-                               font_scale=1.25)
+import seaborn as sns;
 
-color_list = ['cerulean', 'slate blue', 'leaf green', 'scarlet', 'pumpkin']
-sns.set_palette(sns.xkcd_palette(color_list))
+rc = {'figure.figsize':(11.7,8.27),
+      'hatch.color':'#333333'}
+sns.set(rc=rc,
+        font_scale=1.3)
+
+#color_list = ['coffee', 'steel grey', 'raspberry', 'goldenrod', \
+#              'leaf green', 'pumpkin', 'cerulean']
+#sns.set_palette(sns.xkcd_palette(color_list))
+color_palette = sns.color_palette('YlOrBr_r', 7)
+sns.set_palette(color_palette)
+
+alpha = 0.8
+
+pattern_wKinFeats = ''
+pattern_woKinFeats = '.'
+pattern_DCAvecComp = '|'
+pattern_engHFfeats = '/'
+pattern_allHFfeats = '|/'
+
+patch_wKinFeats_DCAvecComp = mpatches.Patch(color=color_palette[0], label='default feats, incl. kinematic feats & DCA vector components', alpha=alpha, hatch=pattern_wKinFeats*2+pattern_DCAvecComp*2)
+patch_wKinFeats_DCAvecComp.set_edgecolor(rc['hatch.color'])
+
+patch_wKinFeats_DCAvecComp_noMassFeat = mpatches.Patch(color=color_palette[1], label='default feats, excl. mass', alpha=alpha, hatch=pattern_wKinFeats*2+pattern_DCAvecComp*2)
+patch_wKinFeats_DCAvecComp_noMassFeat.set_edgecolor(rc['hatch.color'])
+
+patch_wKinFeats = mpatches.Patch(fill=False, hatch=pattern_wKinFeats*3, label='(undotted) with kinematic feats')
+
+patch_woKinFeats = mpatches.Patch(fill=False, hatch=pattern_woKinFeats*3, label='w/o kinematic feats')
+
+patch_DCAvecComp = mpatches.Patch(fill=False, hatch=pattern_DCAvecComp*3, label='DCA vector components only')
+
+patch_engHFfeats = mpatches.Patch(fill=False, hatch=pattern_engHFfeats*3, label='engineered HF feats only')
+
+patch_allHFfeats = mpatches.Patch(fill=False, hatch=pattern_allHFfeats*3, label='DCA vec. comp. & engineered HF feats')
+
+
 
 N = 3
 
@@ -12,29 +46,37 @@ N = 3
 defaultFeats = (98.5, 76.9, 81.7)
 noMassFeat = (98.4, 0., 0.)
 noKinematicFeats = (90.8, 71.1, 71.6)
-wHFfeats = (98.4, 75.9, 0.)
-noKinematicFeats_wHFfeats = (89.5, 69.5, 0.)
+wHFfeats = (98.4, 77.1, 0.)
+noKinematicFeats_wHFfeats = (80.2, 62.8, 0.)
+wAllHFfeats = (98.3, 78.7, 0.)
+wAllHFfeats_noKinematicFeats = (92.7, 72.0, 0.)
 
 ind = np.arange(N) # the x locations for the groups
-width = 0.15 # the width of the bars
+width = 0.11 # the width of the bars
 
 fig, ax = plt.subplots()
-bars_defaultFeats = ax.bar(ind, defaultFeats, width, alpha=.75)
-bars_noMassFeat = ax.bar(ind+width, noMassFeat, width, alpha=.75)
-bars_noKinematicFeats = ax.bar(ind+width*2, noKinematicFeats, width, alpha=.75)
-bars_wHFfeats = ax.bar(ind+width*3, wHFfeats, width, alpha=.75)
-bars_noKinematicFeats_wHFfeats = ax.bar(ind+width*4, noKinematicFeats_wHFfeats, width, alpha=.75)
+bars_defaultFeats = ax.bar(ind, defaultFeats, width, alpha=alpha, hatch=pattern_wKinFeats+pattern_DCAvecComp)
+bars_noMassFeat = ax.bar(ind+width, noMassFeat, width, alpha=alpha, hatch=pattern_wKinFeats+pattern_DCAvecComp)
+bars_noKinematicFeats = ax.bar(ind+width*2, noKinematicFeats, width, alpha=alpha, hatch=pattern_woKinFeats+pattern_DCAvecComp)
+bars_wHFfeats = ax.bar(ind+width*3, wHFfeats, width, alpha=alpha, hatch=pattern_wKinFeats+pattern_engHFfeats)
+bars_noKinematicFeats_wHFfeats = ax.bar(ind+width*4, noKinematicFeats_wHFfeats, width, alpha=alpha, hatch=pattern_woKinFeats+pattern_engHFfeats)
+bars_wAllHFfeats = ax.bar(ind+width*5, wAllHFfeats, width, alpha=alpha, hatch=pattern_wKinFeats+pattern_allHFfeats)
+bars_wAllHFfeats_noKinematicFeats = ax.bar(ind+width*6, wAllHFfeats_noKinematicFeats, width, alpha=alpha, hatch=pattern_woKinFeats+pattern_allHFfeats)
 
 ax.set_ylabel('ROC AUC [%]')
-ax.set_xticks(ind + width*0.5)
-ax.set_xticklabels(('RP::HF','CombBG::HF','NonConvs::HF'))
-ax.legend((bars_defaultFeats[0],
-           bars_noMassFeat[0],
-           bars_noKinematicFeats[0],
-           bars_wHFfeats[0],
-           bars_noKinematicFeats_wHFfeats[0]),
-          ('defaultFeats','noMassFeat','noKinematicFeats','wHFfeats','noKinematicFeats + wHFfeats'),
-          ncol=2)
+ax.set_xticks(ind+width*2.5)
+ax.set_xticklabels(('RP::HF (pair level)','CombBG::HF (pair level)','NonConvs::HF (single-track level)'))
+ax.legend(handles=[patch_wKinFeats_DCAvecComp,patch_wKinFeats_DCAvecComp_noMassFeat, patch_DCAvecComp, patch_engHFfeats, patch_wKinFeats, patch_woKinFeats, patch_allHFfeats])
+#ax.legend((bars_defaultFeats[0],
+#           bars_noMassFeat[0],
+#           bars_noKinematicFeats[0],
+#           bars_wHFfeats[0],
+#           bars_noKinematicFeats_wHFfeats[0],
+#           bars_wAllHFfeats[0],
+#           bars_wAllHFfeats_noKinematicFeats),
+#          ('with kin. feats, DCA vec. comp.','w/o mass feat','w/o kin. feats, DCA vec. comp.','with kin. feats, engineered HF feats','w/o kin. feats, engineered HF feats','with kin. feats, with all HF feats','w/o kin. feats, with all HF feats'),
+#          ncol=2)
+ax.set_ylim([50,120])
 
 def autolabel(rects):
     """
@@ -42,15 +84,18 @@ def autolabel(rects):
     """
     for rect in rects:
         height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width()/2., 1.02*height,
-                '%.1f' % (height),
-                ha='center', va='bottom')
+        if height > 0:
+            ax.text(rect.get_x() + rect.get_width()/2., 1.02*height,
+                    '%.1f' % (height),
+                    ha='center', va='bottom', rotation=90)
 
 autolabel(bars_defaultFeats)
 autolabel(bars_noMassFeat)
 autolabel(bars_noKinematicFeats)
 autolabel(bars_wHFfeats)
 autolabel(bars_noKinematicFeats_wHFfeats)
+autolabel(bars_wAllHFfeats)
+autolabel(bars_wAllHFfeats_noKinematicFeats)
 
 plt.savefig('temp_output/HFstudy_ROCAUC-comparison.png')
 plt.savefig('temp_output/HFstudy_ROCAUC-comparison.pdf')
